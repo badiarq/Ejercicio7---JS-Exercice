01adfd9961ff35eb8f2f19c541b3573377106cdb
Badr OUAHBI <badii_00@hotmail.com>
2022-07-03 21:55:26 +0200

    Grid fixed

--- viewer.js
+++ viewer.js
@@ -23,3 +23,31 @@ async function loadIfc(url) {
 }
 
 loadIfc(projectLink);
+
+function ProjectShortcut(id)
+{
+    if (id == 'ps1'){
+        projectLink = './IFC/01.ifc';
+        loadIfc(projectLink);
+    }
+    else if (id == 'ps2'){
+        projectLink = './IFC/02.ifc/';
+        loadIfc(projectLink);
+    }
+    else if (id == 'ps3'){
+        projectLink = './IFC/03.ifc/';
+        loadIfc(projectLink);
+    }
+    else if (id == 'ps4'){
+        projectLink = './IFC/04.ifc/';
+        loadIfc(projectLink);
+    }
+    else if (id == 'ps5'){
+        projectLink = './IFC/05.ifc/';
+        loadIfc(projectLink);
+    }
+    else {
+        alert('\nThere is no model for this project yet.\nPlease make a selection between Project nº1 and Project nº5.');
+        window.open('./viewer.html', '_self');
+    }
+}
